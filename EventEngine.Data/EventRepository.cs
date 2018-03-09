c5adc2d486428fab8c58cf412f1a467dfb2bcebe
Andrew Duffew <andrew@codeconcepts.co.uk>
2018-03-09 20:46:39 +0000

    Added event data project for eventstore

---
+++ EventEngine.Data/EventRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using EventEngine.Interfaces.Events;
+using EventEngine.Interfaces.Repositories;
+
+namespace EventEngine.Data
+{
+    public class EventStore : IEventStore
+    {
+        public void Add(IEnumerable<IEvent> events)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IEvent> Get(Guid? contextId = null, DateTime? @from = null, IEventType[] eventTypes = null)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
